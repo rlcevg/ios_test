4d281d6b2c24b1fa62facfa0cfb04527c3b0efe6
rlcevg <rlcevg@gmail.com>
2013-06-04 19:38:03 +0300

    ticket:1 Added Person entity

---
+++ iOSapp/iOSapp/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  iOSapp
+//
+//  Created by Evgenij on 6/4/13.
+//  Copyright (c) 2013 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Person : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * surname;
+@property (nonatomic, retain) NSString * bio;
+@property (nonatomic, retain) UIImage * photo;
+
+@end
---
+++ iOSapp/iOSapp/Person.m
@@ -0,0 +1,19 @@
+//
+//  Person.m
+//  iOSapp
+//
+//  Created by Evgenij on 6/4/13.
+//  Copyright (c) 2013 Home. All rights reserved.
+//
+
+#import "Person.h"
+
+
+@implementation Person
+
+@dynamic name;
+@dynamic surname;
+@dynamic bio;
+@dynamic photo;
+
+@end
